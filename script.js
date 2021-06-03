8483d262c3e42b69e1d5b6dd9a1f295626d7af2d
Jaycie McBrayer <jaycielaynee@gmail.com>
2021-06-03 09:32:51 -0500

    additions for added task form and drafting functions

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 
 //Add new tasks from input field to list
 function addTask() {
-
+//storage.setItem()
 };
 
 //Remove tasks from list
 function deleteTask() {
-
+//storage.removeItem()
 };
+
+/* local storage is key, value pairs: in this  instance would? 
+key = task
+value = task input submitted
+Input:
+setItem();
+
+Delete:
+removeItem();
+*/
