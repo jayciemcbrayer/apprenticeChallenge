b3be64619379570cc4b32fef1ca53891c017da41
Jaycie McBrayer <jaycielaynee@gmail.com>
2021-06-04 11:24:50 -0500

    added delete functionality, saving progress and then troubleshooting an error regarding like 98 of my script file

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const taskList = document.querySelector(".task-list");
 //Event Listeners
 taskButton.addEventListener('click', addTask);
 taskList.addEventListener('click', deleteTask);
+document.addEventListener("DOMContentLoaded", getTasks);
 
 //Functions
 
@@ -33,8 +34,6 @@ function addTask(event) {
   taskList.appendChild(tasksDiv);
   //Clear task input value
   taskInput.value = "";
-//Add tasks to local storage
-  //storage.addItem(key, value)
 }
 
 //Remove tasks from list
@@ -44,6 +43,7 @@ function deleteTask(e) {
   //Delete task
   if (task.classList[0] === 'delete-btn') {
     const item = task.parentElement;
+    deleteTask(todo);
     item.remove();
   }
   //storage.removeItem()
@@ -52,13 +52,52 @@ function deleteTask(e) {
 function saveTasksOnLocal(task) {
   //Checks to see if item already exsists in local storage
   let tasks;
+  if (localStorage.getItem('tasks') === null) { //If it's not already stored, it adds the item to local storage
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks')); //loads saved data from local storage to the task list on the page
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getTasks() {
+  let tasks;
   if (localStorage.getItem('tasks') === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  tasks.forEach(task => {
+  //Renders tasks div for items in storage
+  const tasksDiv = document.createElement('div');
+  tasksDiv.classList.add('tasks');
+  //Renders tasks list for items in storage
+  const newTask = document.createElement('li');
+  newTask.innerText = task;
+  newTask.classList.add('task-item');
+  tasksDiv.appendChild(newTask); //This puts the new task list item into the div
+  //Delete button for each item in storage
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = "Delete";
+  deleteButton.classList.add("delete-btn");
+  tasksDiv.appendChild(deleteButton); //This adds the delete buttons into the div
+  //Append together with the task list
+  taskList.appendChild(tasksDiv);
+  });
+}
+
+function deleteTask(task) {
+  //Checks to see if item already exsists in local storage
+  let tasks;
+  if (localStorage.getItem('tasks') === null) { //If it's not already stored, it adds the item to local storage
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks')); //loads saved data from local storage to the task list on the page
+  }
+  const taskIndex = task.children[0].innerText;
+  tasks.splice(tasks.indexOf(taskIndex), 1); //Tells what position of the element to remove and how many that you want to remove
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 /* local storage is key, value pairs: in this  instance would I? 
